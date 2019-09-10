3fec38c35a32ea2e7dccb74cc4350dde1237ecbb
CodeNameKND14 <50086863+CodeNameKND14@users.noreply.github.com>
2019-09-10 10:46:16 -0700

    Update VBA_script.vb

--- VBA_script.vb
+++ VBA_script.vb
@@ -1,96 +1,122 @@
+Sub stock_analysis():
 
+    ' Setting the dimensions
+    Dim total As Double
+    Dim i As Long
+    Dim change As Single
+    Dim j As Integer
+    Dim start As Long
+    Dim rowCount As Long
+    Dim percentChange As Double
+    Dim days As Integer
+    Dim dailyChange As Single
+    Dim averageChange As Single
+    Dim ws As Worksheet
 
-1. 
-Sub Code_one()
-Dim Ticker As String
-Dim Total_Volume As Double
-Dim Summary_Table As Integer
-Total_Volume = 0
-Summary_Table = 2
-LastRow = Cells(Rows.Count, 1).End(xlUp).Row
+    For Each ws In Worksheets
+    ' Setting the values for each worksheet
+        j = 0
+        total = 0
+        change = 0
+        start = 2
+        dailyChange = 0
 
-Range("I1").Value = "Ticker"
-Range("J1").Value = "Total Stock Volume"
+    ' Setting the title row
+        ws.Range("I1").Value = "Ticker"
+        ws.Range("J1").Value = "Yearly Change"
+        ws.Range("K1").Value = "Percent Change"
+        ws.Range("L1").Value = "Total Stock Volume"
+        ws.Range("P1").Value = "Ticker"
+        ws.Range("Q1").Value = "Value"
+        ws.Range("O2").Value = "Greatest % Increase"
+        ws.Range("O3").Value = "Greatest % Decrease"
+        ws.Range("O4").Value = "Greatest Total Volume"
 
-For i = 2 To LastRow
+    ' Gettting the row number of the last row with data
+        rowCount = Cells(Rows.Count, "A").End(xlUp).Row
 
-    If Cells(i + 1, 1).Value <> Cells(i, 1).Value Then
-    
-        Ticker = Cells(i, 1).Value
-        Total_Volume = Total_Volume + Cells(i, 7).Value
-    
-        Range("I" & Summary_Table).Value = Ticker
-        Range("J" & Summary_Table).Value = Total_Volume
-    
-        Summary_Table = Summary_Table + 1
-        Total_Volume = 0
+        For i = 2 To rowCount
 
-    Else
-        Total_Volume = Total_Volume + Cells(i, 7).Value
+            ' If ticker changes then print results
+            If ws.Cells(i + 1, 1).Value <> ws.Cells(i, 1).Value Then
 
-    End If
-Next i
+                ' Storing results in variables
+                total = total + ws.Cells(i, 7).Value
 
-End Sub
+                ' Handling zero total volume
+                If total = 0 Then
+                    '  Printing the results
+                    ws.Range("I" & 2 + j).Value = Cells(i, 1).Value
+                    ws.Range("J" & 2 + j).Value = 0
+                    ws.Range("K" & 2 + j).Value = "%" & 0
+                    ws.Range("L" & 2 + j).Value = 0
 
+                Else
+                    ' Finding the First non zero starting value
+                    If ws.Cells(start, 3) = 0 Then
+                        For find_value = start To i
+                            If ws.Cells(find_value, 3).Value <> 0 Then
+                                start = find_value
+                                Exit For
+                            End If
+                        Next find_value
+                    End If
 
+                    ' Calculating Changes
+                    change = (ws.Cells(i, 6) - ws.Cells(start, 3))
+                    percentChange = Round((change / ws.Cells(start, 3) * 100), 2)
 
-2. 
+                    ' Starting the next stock ticker
+                    start = i + 1
 
-Sub Code_two()
-Dim Ticker As String
-Dim Total_Volume As Double
-Dim Summary_Table As Integer
-Dim Yearly_Change As Double
-Dim Percent_Change As Double 
-Dim openstock As Double
-Dim closingstock As Double
-Dim NewLine As Boolean
-Total_Volume = 0
-Summary_Table = 2
-LastRow = Cells(Rows.Count, 1).End(xlUp).Row
-NewLine = True
+                    ' Printing the results to a separate worksheet
+                    ws.Range("I" & 2 + j).Value = ws.Cells(i, 1).Value
+                    ws.Range("J" & 2 + j).Value = Round(change, 2)
+                    ws.Range("K" & 2 + j).Value = "%" & percentChange
+                    ws.Range("L" & 2 + j).Value = total
 
-Range("I1").Value = "Ticker"
-Range("J1").Value = "Yearly Change"
-Range("K1").Value = "Percent Change"
-Range("L1").Value = "Total Stock Volume"
+                    ' Coloring positives green and negatives red
+                    Select Case change
+                        Case Is > 0
+                            ws.Range("J" & 2 + j).Interior.ColorIndex = 4
+                        Case Is < 0
+                            ws.Range("J" & 2 + j).Interior.ColorIndex = 3
+                        Case Else
+                            ws.Range("J" & 2 + j).Interior.ColorIndex = 0
+                    End Select
 
-On Error Resume Next
+                End If
 
-For i = 2 To LastRow
+                ' Resetting the variables for new stock ticker
+                total = 0
+                change = 0
+                j = j + 1
+                days = 0
+                dailyChange = 0
 
-    If Cells(i + 1, 1).Value = Cells(i, 1).Value And NewLine = True Then
-        openstock = Cells(i, 3).Value
-        NewLine = False
+            ' If ticker is still the same adding results
+            Else
+                total = total + ws.Cells(i, 7).Value
 
-    ElseIf Cells(i + 1, 1).Value <> Cells(i, 1).Value Then
-    
-        Ticker = Cells(i, 1).Value
-        Total_Volume = Total_Volume + Cells(i, 7).Value
-        closingstock = Cells(i, 6).Value
-        Yearly_Change = closingstock - openstock
-        Percent_Change = (Yearly_Change / (openstock)) * 100    ' I think this i right
+            End If
 
-        Range("I" & Summary_Table).Value = Ticker
-        Range("J" & Summary_Table).Value = Yearly_Change
-        Range("K" & Summary_Table).Value = Percent_Change
-        Range("L" & Summary_Table).Value = Total_Volume
-        If Range("J" & Summary_Table).Value < 0 Then
-            Range("J" & Summary_Table).Interior.ColorIndex = 3
-        Else
-           Range("J" & Summary_Table).Interior.ColorIndex = 4
-        End If
+        Next i
 
-        Summary_Table = Summary_Table + 1
-        Total_Volume = 0
-        
-        NewLine = True
+        ' Taking the max and min and place them in a separate part in the worksheet
+        ws.Range("Q2") = "%" & WorksheetFunction.Max(ws.Range("K2:K" & rowCount)) * 100
+        ws.Range("Q3") = "%" & WorksheetFunction.Min(ws.Range("K2:K" & rowCount)) * 100
+        ws.Range("Q4") = WorksheetFunction.Max(ws.Range("L2:L" & rowCount))
 
-    Else
-        Total_Volume = Total_Volume + Cells(i, 7).Value
+        ' Returning one less because header row not a factor
+        increase_number = WorksheetFunction.Match(WorksheetFunction.Max(ws.Range("K2:K" & rowCount)), ws.Range("K2:K" & rowCount), 0)
+        decrease_number = WorksheetFunction.Match(WorksheetFunction.Min(ws.Range("K2:K" & rowCount)), ws.Range("K2:K" & rowCount), 0)
+        volume_number = WorksheetFunction.Match(WorksheetFunction.Max(ws.Range("L2:L" & rowCount)), ws.Range("L2:L" & rowCount), 0)
 
-    End If
-Next i
+        ' Final ticker symbol for  total, greatest % of increase and decrease, and average
+        ws.Range("P2") = ws.Cells(increase_number + 1, 9)
+        ws.Range("P3") = ws.Cells(decrease_number + 1, 9)
+        ws.Range("P4") = ws.Cells(volume_number + 1, 9)
+
+    Next ws
 
 End Sub
